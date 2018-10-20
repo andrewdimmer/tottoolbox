c30320627b8e5f570f2c301848ce18444e78631d
andrewdimmer <43498863+andrewdimmer@users.noreply.github.com>
2018-10-20 18:33:37 -0400

    Added a button to populate the address from the current location

--- scripts/mappagescripts.js
+++ scripts/mappagescripts.js
@@ -13,3 +13,18 @@ function returnToMap() {
     document.getElementById("inputContainerSuper").style.display = "none";
     document.getElementById("mapContainerSuper").style.display = "block";
 }
+
+function prepopulateAddress() {
+    console.log("starting");
+    getUserLocation(function(userLocation) {
+        getAddressOfLocation(userLocation, function(response) {
+            // console.log(response);
+            var address = response.Response.View[0].Result[0].Location.Address;
+            console.log(address);
+            document.getElementById("city").value = address.City;
+            document.getElementById("state").value = address.AdditionalData[1].value;
+            document.getElementById("zipcode").value = address.PostalCode;
+            addGoodMessage("Retrieved City, State, and Zip Code")
+        }, function(error) {addBadMessage(error);});
+    });
+}
