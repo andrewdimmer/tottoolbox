3c83e2e7a473de226da823fd6a7970cf607289a5
andrewdimmer <43498863+andrewdimmer@users.noreply.github.com>
2018-10-20 16:18:13 -0400

    Added the panes for filter and insert

---
+++ scripts/mappagescripts.js
@@ -0,0 +1,9 @@
+function toggleFilterPane() {
+    document.getElementById("filterContainerSuper").style.display = "block";
+    document.getElementById("mapContainerSuper").style.display = "none";
+}
+
+function toggleInputPane() {
+    document.getElementById("inputContainerSuper").style.display = "block";
+    document.getElementById("mapContainerSuper").style.display = "none";
+}
